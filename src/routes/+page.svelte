ab98385a429eb92d24174c80b220d15b84b2a40e
Mirau <133158735+AmirageTheMirage@users.noreply.github.com>
2024-01-29 16:20:56 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,21 @@
-<div class="mainDiv">
+<body>
 
-    <h1>This is a Website</h1>
-    <p>Visit <a href="https://de.wikipedia.org/wiki/Website">Wikipedia</a> to learn more.</p>
-</div>
+    <div class="mainDiv">
+    
+        <h1>This is a Website</h1>
+        <p>Visit <a href="https://de.wikipedia.org/wiki/Website">Wikipedia</a> to learn more.</p>
+    </div>
+    
+</body>
+    
 
 
 <style>
     h1 {
         color: brown;
     }
-    .mainDiv {
+    body {
         background-color: aqua;
     }
+
 </style>
